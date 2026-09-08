d33b17dc1804e566eee9db43feb05395436bb389
s95722462-cell <s95722462@gmail.com>
2026-09-08 09:34:53 +0900

    3

--- js/invoice.js
+++ js/invoice.js
@@ -136,7 +136,7 @@ const InvoiceModule = (() => {
         <div class="inv-info-line-pair"><span class="inv-info-half"><b>상호:</b> ${escapeHtml(party.company || party.name || '-')}</span><span class="inv-info-half"><b>대표자:</b> ${escapeHtml(party.ceo || '-')}</span></div>
         <div class="inv-info-line"><b>사업자번호:</b> ${escapeHtml(party.bizno || '-')}</div>
         <div class="inv-info-line-pair"><span class="inv-info-half"><b>전화번호:</b> ${escapeHtml(party.tel || '-')}</span><span class="inv-info-half"><b>팩스번호:</b> ${escapeHtml(party.fax || '-')}</span></div>
-        <div class="inv-info-line"><b>주소:</b> ${escapeHtml(party.addr || '-')}</div>
+        <div class="inv-addr-line"><span class="inv-addr-label"><b>주소:</b></span><span class="inv-addr-value">${escapeHtml(party.addr || '-')}</span></div>
       </div>`;
   }
 
